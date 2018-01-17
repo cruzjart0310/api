34ba43853a67a9ee75b4ad5db43c5c78d1b9a008
Arturo Cruz <cruzjart@gmail.com>
2018-01-17 08:10:46 -0500

    Se añade seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,28 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+       	// $this->call(UsersTableSeeder::class);
+       	DB::table('users')->insert(['name' => 'Arturo Cruz', 'email' => 'jcruz@abrhil.com', 'password' => 'calabaza', 'remember_token' => '']);
+
+       	DB::table('user_profiles')->insert(['user_id' => '1', 'address' => 'SM 102', 'ocupation' => 'Dev Backend']);
+
+       	DB::table('roles')->insert(['name' => 'Role 1', 'description' => 'Desc']);
+       	DB::table('roles')->insert(['name' => 'Role 2', 'description' => 'Desc']);
+       	DB::table('roles')->insert(['name' => 'Role 3', 'description' => 'Desc']);
+
+       	DB::table('permissions')->insert(['name' => 'Permission 1', 'description' => 'Desc']);
+       	DB::table('permissions')->insert(['name' => 'Permission 2', 'description' => 'Desc']);
+       	DB::table('permissions')->insert(['name' => 'Permission 3', 'description' => 'Desc']);
+
+       	DB::table('roles_permissions')->insert(['role_id' => '1', 'permission_id' => '1']);
+       	DB::table('roles_permissions')->insert(['role_id' => '1', 'permission_id' => '2']);
+       	DB::table('roles_permissions')->insert(['role_id' => '1', 'permission_id' => '3']);
+       	DB::table('roles_permissions')->insert(['role_id' => '2', 'permission_id' => '1']);
+       	DB::table('roles_permissions')->insert(['role_id' => '2', 'permission_id' => '2']);
+       	DB::table('roles_permissions')->insert(['role_id' => '3', 'permission_id' => '1']);
+
+       	DB::table('users_permissions')->insert(['user_id' => '1', 'permission_id' => '1']);
+       	DB::table('users_permissions')->insert(['user_id' => '1', 'permission_id' => '2']);
+       	DB::table('users_permissions')->insert(['user_id' => '1', 'permission_id' => '3']);
     }
 }
