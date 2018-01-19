a804cb98cea14327f837f1482b8bd7f22389983d
Arturo Cruz <cruzjart@gmail.com>
2018-01-19 12:33:23 -0500

    Se añade consulta en Model
    Forma de obtener datos desde un Model

--- app/User.php
+++ app/User.php
@@ -26,4 +26,17 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+    
+     //Obtenemos el grupo y la companie de el cliente que creo el ticket
+    public static function getGroup($id)
+    {
+        $data_office = self::select('groups.name as group','companies.name as companie')
+                        ->join('companie_user', 'companie_user.companie_id', '=' , 'companies.id')
+                        ->join('groups', 'groups.id', '=', 'companies.group_id')
+                        ->join('tickets', 'tickets.user_creation', '=', 'companie_user.user_id')
+                        ->where('tickets.id', $id)
+                        ->first();
+
+        return $data_office;
+    }
 }
