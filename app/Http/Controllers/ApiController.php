29ad223ccdb4667e9d2d0f99251470026622e18f
Arturo Cruz <cruzjart@gmail.com>
2018-03-04 14:15:20 -0500

    Se elimina dependencia del paquete Hashid

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -2,12 +2,11 @@
 
 namespace App\Http\Controllers;
 
-use App\Traits\HashIdTrait;
 use App\Traits\ApiResponser;
 use Illuminate\Http\Request;
 
 class ApiController extends Controller
 {
-    use ApiResponser, HashIdTrait;
+    use ApiResponser;
 
 }
